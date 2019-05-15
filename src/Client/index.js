4c43aaaf00142a91c1bfca327744eed83a6a7451
Ben Clarke <ben@benclarke.me>
2019-05-15 18:36:40 +0000

    Update index.js

--- src/Client/index.js
+++ src/Client/index.js
@@ -26,6 +26,7 @@ const authLink = setContext((_, { headers }) => {
 
 const cache = new InMemoryCache({
   fragmentMatcher,
+  dataIdFromObject: object => object.uuid || null,
 });
 
 const client = new ApolloClient({
