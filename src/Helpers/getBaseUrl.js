7772ee66c7e52c2c2915102e8e0e4ec1f31e2969
Ben Clarke <ben@benclarke.me>
2019-05-17 19:06:38 +0000

    Update getBaseUrl.js

--- src/Helpers/getBaseUrl.js
+++ src/Helpers/getBaseUrl.js
@@ -7,8 +7,7 @@ const getBaseUrl = () => {
   let path;
 
   if (typeof window !== 'undefined') {
-    path = 'http://maran.atalanta.bysh.me';
-    // path = `${window.location.protocol}//${window.location.host}`;
+    path = `${window.location.protocol}//${window.location.host}`;
   } else {
     return false;
   }
