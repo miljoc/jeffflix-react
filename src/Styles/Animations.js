4063464fe56727f2f70d7d4bec606080a9d73a05
Ben Clarke <b.clarke@gymshark.com>
2019-06-03 10:52:01 +0100

    Update Fomatting Styles

--- src/Styles/Animations.js
+++ src/Styles/Animations.js
@@ -1,11 +1,5 @@
 import { keyframes } from 'styled-components';
-import {
-  fadeIn,
-  fadeOut,
-  rotateIn,
-  headShake,
-  zoomIn,
-} from 'react-animations';
+import { fadeIn, fadeOut, rotateIn, headShake, zoomIn } from 'react-animations';
 
 const aFadeIn = keyframes`${fadeIn}`;
 const aZoomIn = keyframes`${zoomIn}`;
@@ -14,21 +8,14 @@ const aRotate = keyframes`${rotateIn}`;
 const aHeadShake = keyframes`${headShake}`;
 
 const aFadeInUp = keyframes`
-  0% { 
-    opacity:0; 
+  0% {
+    opacity:0;
     transform: translateY(1rem);
   }
-  100% { 
-    opacity:1; 
+  100% {
+    opacity:1;
     transform: translateY(0);
   }
 `;
 
-export {
-  aFadeIn,
-  aZoomIn,
-  aFadeOut,
-  aRotate,
-  aHeadShake,
-  aFadeInUp,
-};
+export { aFadeIn, aZoomIn, aFadeOut, aRotate, aHeadShake, aFadeInUp };
