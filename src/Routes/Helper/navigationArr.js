0183944f0cc81193d3df7ff81461a16b12b4a841
Ben Clarke <b.clarke@gymshark.com>
2019-06-03 11:07:40 +0100

    Update Standards Routes

--- src/Routes/Helper/navigationArr.js
+++ src/Routes/Helper/navigationArr.js
@@ -1,38 +1,38 @@
 const navigationArr = [
-  {
-    name: 'Libraries',
-    id: 'libraries',
-    admin: false,
-    links: [
-      {
-        name: 'Dashboard',
-        to: '/dashboard',
-        id: 'dashboard',
-      },
-      {
-        name: 'Movies',
-        to: '/movies',
-        id: 'movies',
-      },
-      {
-        name: 'TV Shows',
-        to: '/series',
-        id: 'series',
-      },
-    ],
-  },
-  {
-    name: 'Settings',
-    id: 'settings',
-    admin: true,
-    links: [
-      {
-        name: 'Users',
-        to: '/users',
-        id: 'users',
-      },
-    ],
-  },
+    {
+        name: 'Libraries',
+        id: 'libraries',
+        admin: false,
+        links: [
+            {
+                name: 'Dashboard',
+                to: '/dashboard',
+                id: 'dashboard'
+            },
+            {
+                name: 'Movies',
+                to: '/movies',
+                id: 'movies'
+            },
+            {
+                name: 'TV Shows',
+                to: '/series',
+                id: 'series'
+            }
+        ]
+    },
+    {
+        name: 'Settings',
+        id: 'settings',
+        admin: true,
+        links: [
+            {
+                name: 'Users',
+                to: '/users',
+                id: 'users'
+            }
+        ]
+    }
 ];
 
 export default navigationArr;
