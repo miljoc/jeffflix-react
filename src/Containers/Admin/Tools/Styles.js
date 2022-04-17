808ac545deb9b95f338891585fb7110e99c0813e
Jacob Kossman <jacob.kossman@onyxdesign.net>
2022-04-17 04:39:44 -0700

    Fix mobile styles to be aligned left

--- src/Containers/Admin/Tools/Styles.js
+++ src/Containers/Admin/Tools/Styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { media } from 'Styles/Utils';
 import { HeaderIcon, HeaderIconWrap } from 'Components/Media/MediaHeader/Styles';
 
 export const InnerParagraph = styled.p`
@@ -11,7 +11,8 @@ export const MovieLine = styled.div`
     color: #fff;
     display: flex;
     align-items: center;
-
+    width: 100%;
+    
     small {
         opacity: 0.7;
         font-size: 80%;
@@ -29,6 +30,7 @@ export const MovieLine = styled.div`
     ${HeaderIconWrap} {
         display: flex;
         align-items: center;
+        text-align: left;
         width: 100%;
         padding: 1.2rem 1rem;
         
