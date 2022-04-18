6c2d654722c44ded9d6814c771dd7cccc8721dfd
Jacob Kossman <jacob@kossmn.com>
2022-04-18 01:02:20 -0700

    Empty message when no results, switch paragraphs to use MediaOverview styles

--- src/Containers/Admin/Tools/MatchMovies.jsx
+++ src/Containers/Admin/Tools/MatchMovies.jsx
@@ -8,7 +8,8 @@ import Loading from 'Components/Loading';
 
 import MediaMatch from 'Components/Media/MediaHeader/MediaMatch';
 import ReactTooltip from 'react-tooltip';
-import { InnerParagraph, MovieLine, MoviesWrap } from './Styles';
+import { MediaOverview } from 'Components/Media/Styles';
+import { MovieLine, MoviesWrap } from './Styles';
 
 const MatchMovies = () => {
     const { error, loading, data } = useQuery(FETCH_UNIDENTIFIED_MOVIES);
@@ -16,17 +17,21 @@ const MatchMovies = () => {
     if (loading) return <Loading />;
     if (error) return `Error! ${error.message}`;
 	
-    const { unidentifiedMovieFiles } = data ;
+    const { unidentifiedMovieFiles } = data;
 
     return (
         <InnerContent>
             <ReactTooltip effect="solid" place="right" className="tooltip" />
             <PageHeading>Unidentified Movies</PageHeading>
-            <InnerParagraph>Click a movie below to match and add it to your library</InnerParagraph>
             <MoviesWrap>
-                {unidentifiedMovieFiles.length > 0 && unidentifiedMovieFiles.map(movie => {
-                // console.log(movie.fileName);
-                
+                <MediaOverview>
+                    {unidentifiedMovieFiles.length === 0
+                        ? "No unmatched movies. Good for you!"
+                        : "Click a movie below to match and add it to your library"
+                    }
+                </MediaOverview>
+
+                {unidentifiedMovieFiles.length > 0 && unidentifiedMovieFiles.map(movie => {                
                     return (
                         <MovieLine key={movie.uuid}>
                             <MediaMatch uuid={movie.uuid} type="Movie" name={movie.fileName} file={movie.filePath} />
